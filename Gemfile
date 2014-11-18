c600bc7d96051fafc80eaae4d74bc4f6b95792e0
thr3a <example@example.com>
2014-11-18 14:04:10 +0900

    start-sinatraに合わせる

--- Gemfile
+++ Gemfile
@@ -1,11 +1,10 @@
-# A sample Gemfile
 source "https://rubygems.org"
 
-# gem "rails"
 gem "sinatra"
+gem 'sinatra-contrib'
 gem "slim"
-gem "omniauth-twitter"
+gem "unicorn"
+gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
 
-group :development do
-	gem 'sinatra-contrib'
-end
+gem "omniauth-twitter"
+gem "twitter"
--- app.rb
+++ app.rb
@@ -1,13 +1,41 @@
 require 'bundler'
 Bundler.require
-require "sinatra/reloader" if development?
+
+#development or production
+set :environment, :development
+#set :environment, :production
+
+CKEY = '1uAH8P1AbyBg1wdKQ3BVDXdKe'
+CSEC = 'q9YpP6yt7vIMEhed0NFnk63ZwtJPTwoysrItyNZUtFr7HQN7ov'
+
+configure :development do
+	set :server, 'webrick'
+	require 'sinatra/reloader'
+	Slim::Engine.set_default_options :pretty => true
+end
 
 configure do
-	enable :sessions
+	register Sinatra::AssetPack
+	assets do
+		serve '/js', from: 'assets/js'
+		serve '/css', from: 'assets/css'
 
+		js :application, [
+			'/js/*.js'
+		]
+
+		css :application, [
+			'/css/*.css'
+		]
+
+		js_compression :closure, :level => "SIMPLE_OPTIMIZATIONS"
+		css_compression :sass
+	end
+
+	enable :sessions
 	use OmniAuth::Builder do
-		CONSUMER_KEY = ''
-		CONSUMER_SECRET = ''
+		CONSUMER_KEY = CKEY
+		CONSUMER_SECRET = CSEC
 		provider :twitter, CONSUMER_KEY, CONSUMER_SECRET
 	end
 end
@@ -21,6 +49,8 @@ end
 get '/auth/twitter/callback' do
 	session[:userid] = env['omniauth.auth']['uid']
 	session[:username] = env['omniauth.auth']['info']['nickname']
+	session[:token] = env['omniauth.auth'][:credentials][:token]
+	session[:secret] = env['omniauth.auth'][:credentials][:secret]
 	redirect to('/')
 end
 
@@ -41,5 +71,20 @@ get '/' do
 		@userid = session[:userid]
 		@username = session[:username]
 		slim :logined
+	end
+end
+
+post '/tweet' do
+	if current_user
+		slim :prelogin
+	else
+		client = Twitter::REST::Client.new do |config|
+			config.consumer_key = CKEY
+			config.consumer_secret = CSEC
+			config.access_token = session[:token]
+			config.access_token_secret = session[:secret]
+		end
+		client.update "Hello! " + Time.now.strftime("%m-%d %H:%M:%S")
+		redirect to('/')
 	end
 end
